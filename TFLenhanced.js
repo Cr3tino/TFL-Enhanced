dd0937381f1d1f99575579d31ec5106b8de538c7
thedark1337 <1337alexpham@gmail.com>
2013-09-06 11:39:12 -0500

    spacing fix

--- TFLenhanced.js
+++ TFLenhanced.js
@@ -60,7 +60,7 @@ TFLEnhancedModel = require('app/base/Class').extend({
         Lang.ui.buttonDJQuit = "http://i.imgur.com/i4YkTFC.png";
         Lang.ui.buttonDJPlayShort = "http://i.imgur.com/SqU01C6.png";
         Lang.rollover.host = "The Boss"
-        Lang.messages.modKick ="Bass Cannoned %NAME% To The Sun."
+        Lang.messages.modKick =" Bass Cannoned %NAME% To The Sun."
         Lang.dialog.kickUserPrompt = "Are you sure you want to Bass Cannon %NAME% to the sun for 60 minutes? You can optionally provide a reason to show to them."
         Lang.rollover.modKick = "Bass Cannon To Sun"
         Lang.chat.help = "<strong>Chat Commands:</strong><br/>/em &nbsp; <em>Emote</em><br/>/me &nbsp; <em>Emote</em><br/>/clear &nbsp; <em>Clear Chat History</em><br/>/cap # &nbsp; <em>Limits the number of avatars rendered (1-200)</em><br/>/ts # &nbsp; <em>Chat timestamps (12, 24, 0)</em><br />/emoji on (or off) <em>Enable/disable Emojis</em><br /> /strobe on/off &nbsp; <em>Strobe light on/off</em><br /> /rave on/off &nbsp; <em>Lights out on/off</em><br />/close &nbsp; <em>Remove TFL Enhanced script</em> <br /> /Avatar # &nbsp; <em> change Halloween Avatars ( # = 1-13)</em> <br /> /Auto On &nbsp; <em> plugbot load </em>"
@@ -143,7 +143,7 @@ TFLEnhancedModel = require('app/base/Class').extend({
         Lang.rollover.fans = "fans"
         Lang.rollover.host = "Host"
         Lang.alerts.updateMesage ="plug.dj has been updated and requires a refresh. Click OK to refresh the page."
-        Lang.messages.modKick ="kicked %NAME% out of the room."
+        Lang.messages.modKick =" kicked %NAME% out of the room."
         Lang.dialog.kickUserPrompt = "Are you sure you want to kick %NAME% from the room for 60 minutes? You can optionally provide a reason to show to them."
         Lang.rollover.modKick = "Kick From Room"
         Lang.chat.help = "<strong>Chat Commands:</strong><br/>/em &nbsp; <em>Emote</em><br/>/me &nbsp; <em>Emote</em><br/>/clear &nbsp; <em>Clear Chat History</em><br/>/cap # &nbsp; <em>Limits the number of avatars rendered (1-200)</em><br/>/ts # &nbsp; <em>Chat timestamps (12, 24, 0)</em><br/>/emoji on (or off) <em>Enable/disable Emojis</em>"        
